14ec7ca174a214d9b7d2d7beca606d8be0c8ff0a
jyp930 <jyp930@daum.net>
2020-11-10 23:54:01 +0900

    styled-components 테스트

--- src/pages/jyp01930.js
+++ src/pages/jyp01930.js
@@ -1,17 +1,23 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import styled from "styled-components"
+
+const Test = styled.div`
+  color: red;
+`
 
 export default function Jyp01930() {
   return (
-    <div>
+    <Test>
       <Helmet>
         <meta charSet="utf-8" />
         <meta property="og:title" content="생일생일" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://snufestival.com/mascot.jpg" />
         <meta property="og:description" content="해피버스데이" />
-        <title>생일 축하해 😇</title>
+        <title>창민아 생일 축하한돠~~</title>
       </Helmet>
-    </div>
+      생일축하~~
+    </Test>
   );
 }
