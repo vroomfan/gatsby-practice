6d1aa58d500ac71aa10f8764adc25b826702960b
jyp0808 <jyp0808@loplat.com>
2020-11-12 18:41:09 +0900

    feat: circle 복권 완성

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,37 +2,10 @@ import React, { useEffect } from "react"
 import styled from "styled-components"
 import Layout from "../layout/layout"
 
-export default function Home() {
-  useEffect(() => {
-    initialize();
-  }, []);
-
-  console.log(document.documentElement.clientWidth);
-  console.log(document.documentElement.offsetWidth);
-  console.log(window.outerWidth);
-
-  return (
-    <Layout>
-      <Container id="Container">
-        <InitialCircle id="InitialCircle" />
-      </Container>
-    </Layout>
-  );
-}
-
+const fullHeight = document.documentElement.clientHeight;
+const fullWidth = document.documentElement.clientWidth;
+const fullRadius = Math.min(fullWidth, fullHeight);
 let containerElement = null;
-let circleIndex = 0;
-
-function initialize() {
-  containerElement = document.getElementById('Container');
-  const initialCircleElement = document.getElementById('InitialCircle');
-
-
-}
-
-function addCircle() {
-
-}
 
 const Container = styled.div`
   width: 100vh;
@@ -41,9 +14,67 @@ const Container = styled.div`
   position: relative;
 `;
 
-const InitialCircle = styled.div`
-  width: 100%;
-  height: 100%;
-  border-radius: 50%;
-  background-color: lightblue;
-`;
+export default function Home() {
+  useEffect(() => {
+    initialize();
+  }, []);
+
+  return (
+    <Layout>
+      <Container id="Container">
+        <div
+          id="InitialCircle"
+          data-x={fullRadius / 2}
+          data-y={fullRadius / 2}
+          data-radius={fullRadius / 2}
+        />
+      </Container>
+    </Layout>
+  );
+}
+
+function initialize() {
+  containerElement = document.getElementById('Container');
+  const initialCircleElement = document.getElementById('InitialCircle');
+
+  const { x: stringX, y: stringY, radius: stringRadius } =  initialCircleElement.dataset;
+  const [x, y, radius] = [Number(stringX), Number(stringY), Number(stringRadius)];
+
+  addCircle(x, y, radius);
+}
+
+function addCircle(x, y, radius) {
+  if (radius < 10) return ;
+
+  const newCircle = document.createElement('div');
+  newCircle.setAttribute('style', '' +
+    `top: ${y - radius}px;` +
+    `left: ${x - radius}px;` +
+    `width: ${radius * 2}px;` +
+    `height: ${radius * 2}px;` +
+    'position: absolute; border-radius: 50%; background-color: lightblue;'
+  );
+  newCircle.addEventListener('mousemove', () => {
+    splitCircle(x, y, radius);
+    newCircle.remove();
+  });
+  containerElement.appendChild(newCircle);
+}
+
+function splitCircle(x, y, radius) {
+  const reductionRatio = 1 / (1 + 2**(1/2));
+  const newRadius = reductionRatio * radius;
+  const secondReductionRatio = (2**(1/2) - 1);
+  const secondNewRadius = secondReductionRatio * newRadius;
+
+  const distance = newRadius * (2**(1/2));
+  addCircle(x - distance, y, newRadius);
+  addCircle(x + distance, y, newRadius);
+  addCircle(x, y - distance, newRadius);
+  addCircle(x, y + distance, newRadius);
+  addCircle(x, y, secondNewRadius);
+  addCircle(x - (2**(1/2) * newRadius), y - (2**(1/2) * newRadius), secondNewRadius);
+  addCircle(x - (2**(1/2) * newRadius), y + (2**(1/2) * newRadius), secondNewRadius);
+  addCircle(x + (2**(1/2) * newRadius), y - (2**(1/2) * newRadius), secondNewRadius);
+  addCircle(x + (2**(1/2) * newRadius), y + (2**(1/2) * newRadius), secondNewRadius);
+}
