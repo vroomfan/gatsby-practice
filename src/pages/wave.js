7680f6df99d2a6426546e8f96d621395605a544e
jyp0808 <jyp0808@loplat.com>
2020-11-23 12:56:47 +0900

    feat: interactive developer 웨이브 효과

---
+++ src/pages/wave.js
@@ -0,0 +1,159 @@
+import React, { useEffect } from "react"
+
+export default function WaveCanvas() {
+  useEffect(() => {
+    new App();
+  }, []);
+
+  return (
+    <div
+      id="CanvasWrapper"
+      style={{ width: '50vw', height: '50vh' }}
+    />
+  );
+}
+
+class App {
+  constructor() {
+    this.canvas = document.createElement('canvas');
+    this.ctx = this.canvas.getContext('2d');
+
+    this.wrapper = document.getElementById('CanvasWrapper');
+    this.wrapper.appendChild(this.canvas);
+
+    this.waveGroup = new WaveGroup();
+
+    window.addEventListener('resize', this.resize.bind(this), false);
+    this.resize();
+
+    requestAnimationFrame(this.animate.bind(this));
+  }
+
+  resize() {
+    this.stageWidth = this.wrapper.clientWidth;
+    this.stageHeight = this.wrapper.clientHeight;
+
+    this.canvas.width = this.stageWidth * 2;
+    this.canvas.height = this.stageHeight * 2;
+    this.ctx.scale(2, 2);
+
+    this.waveGroup.resize(this.stageWidth, this.stageHeight);
+  }
+
+  animate(t) {
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+    this.waveGroup.draw(this.ctx);
+    requestAnimationFrame(this.animate.bind(this));
+  }
+}
+
+class Point {
+  constructor(index, x, y) {
+    this.x = x;
+    this.y = y;
+    this.fixedY = y;
+    this.speed = 0.1;
+    this.cur = index;
+    this.max = Math.random() * 100 + 100;
+  }
+
+  update() {
+    this.cur += this.speed;
+    this.y = this.fixedY + (Math.sin(this.cur) * this.max);
+  }
+}
+
+class Wave {
+  constructor(index, totalPoints, color) {
+    this.index = index;
+    this.totalPoints = totalPoints;
+    this.color = color;
+    this.points = [];
+  }
+
+  resize(stageWidth, stageHeight) {
+    this.stageWidth = stageWidth;
+    this.stageHeight = stageHeight;
+
+    this.centerX = stageWidth / 2;
+    this.centerY = stageHeight / 2;
+
+    this.pointGap = this.stageWidth / (this.totalPoints - 1);
+
+    this.init();
+  }
+
+  init() {
+    this.points = [];
+
+    for (let i = 0; i < this.totalPoints; i++) {
+      this.points[i] = new Point(
+        this.index + i,
+        this.pointGap * i,
+        this.centerY,
+      );
+    }
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+
+    let prevX = this.points[0].x;
+    let prevY = this.points[0].y;
+
+    ctx.moveTo(prevX, prevY);
+
+    for (let i = 1; i < this.totalPoints; i++) {
+      if (i < this.totalPoints - 1) {
+        this.points[i].update();
+      }
+      const cx = (prevX + this.points[i].x) / 2;
+      const cy = (prevY + this.points[i].y) / 2;
+
+      ctx.quadraticCurveTo(prevX, prevY, cx, cy);
+
+      prevX = this.points[i].x;
+      prevY = this.points[i].y;
+    }
+
+    ctx.lineTo(prevX, prevY);
+    ctx.lineTo(this.stageWidth, this.stageHeight);
+    ctx.lineTo(this.points[0].x, this.stageHeight);
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+class WaveGroup {
+  constructor() {
+    this.totalWaves = 3;
+    this.totalPoints = 6;
+
+    this.color = ['rgba(255, 0, 0, 0.4)', 'rgba(255, 255, 0, 0.4)', 'rgba(0, 255, 255, 0.4)'];
+
+    this.waves = [];
+
+    for (let i = 0; i < this.totalWaves; i++) {
+      this.waves[i] = new Wave(
+        i,
+        this.totalPoints,
+        this.color[i],
+      );
+    }
+  }
+
+  resize(stageWidth, stageHeight) {
+    for (let i = 0; i < this.totalWaves; i++) {
+      const wave = this.waves[i];
+      wave.resize(stageWidth, stageHeight);
+    }
+  }
+
+  draw(ctx) {
+    for (let i = 0; i < this.totalWaves; i++) {
+      const wave = this.waves[i];
+      wave.draw(ctx);
+    }
+  }
+}
