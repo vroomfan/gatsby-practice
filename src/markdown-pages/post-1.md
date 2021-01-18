c9d27f879ca61ff50df83817e46e600206da999d
jyp0808 <jyp0808@loplat.com>
2021-01-18 12:57:58 +0900

    feat: graphql 실험

---
+++ src/markdown-pages/post-1.md
@@ -0,0 +1,10 @@
+---
+slug: "/blog/my-first-post"
+date: "2019-05-04"
+title: "My first blog post"
+---
+
+# HEAD
+## HEAD2
+- 11
+- 22
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,31 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default function Home() {
+export default function Home({ data }) {
+  const { markdownRemark } = data
+  const { frontmatter, html } = markdownRemark
+
   return (
     <div>
-      HOME
+      <h1>{frontmatter.title}</h1>
+      <h2>{frontmatter.date}</h2>
+      <div
+        className="blog-post-content"
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
     </div>
   );
 }
+
+export const pageQuery = graphql`
+  query {
+    markdownRemark {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        slug
+        title
+      }
+    }
+  }
+`
